40aab39159abf4398064f7066e720d4bf9f2eef0
Aakeefah Jones <aakeefahj@gmail.com>
2023-11-09 09:17:12 +0200

    done

--- script/code.js
+++ script/code.js
@@ -1,7 +1,20 @@
 const btnSubmit =
-document.getElementById('btnClick')
+document.getElementById('btnSubmit')
+btnSubmit.addEventListener('click', calculateResult)
 
-let numb1
-let numb2
-console.log(+numb1 + +numb2);
+let numb1 = document.getElementById('numb1')
+let numb2 = document.getElementById('numb2')
 
+function calculateResult(){
+    let total = +numb1.value + +numb2.value
+    let sum = document.getElementById('output')
+    sum.innerHTML = total
+}
+
+/*
+1. Get the button and input elements reference and 
+store them on a specific variable
+2. Create a function that will perform the addition logic 
+and display the sum on a label (id -> output)
+
+*/
